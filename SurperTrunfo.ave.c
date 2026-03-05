246d75cc41df6e10a75f49807b70dea5eb00d805
jczzk <santosjaciel710@icloud.com>
2026-03-05 19:29:06 -0300

    Update SurperTrunfo.ave.c

--- SurperTrunfo.ave.c
+++ SurperTrunfo.ave.c
@@ -6,29 +6,29 @@ int main() {
     // CARTA 1
     // =========================
 
-    char estado_1 = 'A';
-    char codigo_Carta_1[5] = "A01";
-    char nome_Cidade_1[50] = "Rio de Janeiro";
-    int populacao_1 = 11900000;                // Estimativa 2026
-    float area_1 = 1200.25;                    // km²
-    float pib_1 = 1.5;                         // Trilhões de reais (Estimativa 2026)
-    int pontos_turisticos_1 = 45; 
-    float densidade_populacional_1 = 5.170;    // Hab/km²
-    float pib_per_capita_1 = 2.794;            // De reais (Estimativa 2026)
+    char estado_1;
+    char codigo_Carta_1[5];
+    char nome_Cidade_1[50];
+    int populacao_1;                      // Estimativa 2026
+    float area_1;                         // Km²
+    float pib_1;                          // Trilhões de reais (Estimativa 2026)
+    int pontos_turisticos_1; 
+    float densidade_populacional_1;       // Habitantes/km²
+    float pib_per_capita_1;               // De reais (Estimativa 2026)
 
     // =========================
     // CARTA 2
     // =========================
 
-    char estado_2 = 'B';
-    char codigo_Carta_2[5] = "B02";
-    char nome_Cidade_2[50] = "São Paulo";
-    int populacao_2 = 11950000;
-    float area_2 = 1521.11;
-    float pib_2 = 4.3;
-    int pontos_turisticos_2 = 60;
-    float densidade_populacional_2 = 7.600;
-    float pib_per_capita_2 = 3.090;
+    char estado_2;
+    char codigo_Carta_2[5];
+    char nome_Cidade_2[50];
+    int populacao_2;                      // Estimativa 2026
+    float area_2;                         // Km²
+    float pib_2;                          // Trilhões de reais (Estimativa 2026)
+    int pontos_turisticos_2;
+    float densidade_populacional_2;       // Habitantes/km²
+    float pib_per_capita_2;               // De reais (Estimativa 2026)
 
 
     // =============================
@@ -130,6 +130,7 @@ int main() {
 
     return 0;
 }
+
 
 
 
