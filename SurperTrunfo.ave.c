1755331b74bd4b7fda6ec64a850dcc9972d5a105
jczzk <santosjaciel710@icloud.com>
2026-03-05 11:49:39 -0300

    Update SurperTrunfo.ave.c

--- SurperTrunfo.ave.c
+++ SurperTrunfo.ave.c
@@ -39,14 +39,32 @@ int main() {
     // =============================
 
     printf("===== CARTA 1 =====\n");
+    
     printf("Estado: %c\n", estado_1);
+    scanf(" %c", &estado_1);
+    
     printf("Código: %s\n", codigo_Carta_1);
+    scanf(" %s", codigo_Carta_1);
+    
     printf("Nome da Cidade: %s\n", nome_Cidade_1);
+    scanf(" %c[^\n]", nome_Cidade_1);
+    
     printf("População: %d Milhões de pessoas\n", populacao_1);
+    scanf (" %d", &populacao_1);
+    
     printf("Área: %.2f Km²\n", area_1);
+    scanf ("%f", &area_1);
+    
+    
     printf("PIB: %.1f Trilhões de Reais\n", pib_1);
+    scanf ("%f", &pib_1);
+    
     printf("Número de Pontos Turísticos: %d\n", pontos_turisticos_1);
-    printf("Densidade Populacional: %.3f  Habitantes/km²\n", densidade_populacional_1);
+    scanf ("%d", &pontos_turiticos_1);
+    
+    printf("Densidade Populacional: %.3f  Habitantes/km²\n", densidade_populacional_1); 
+    
+    
     printf("PIB Per Capital: %.3f Reais\n\n", pib_per_capita_1);
     
 
@@ -67,14 +85,31 @@ int main() {
     // =============================
 
     printf("===== CARTA 2 =====\n");
+    
     printf("Estado: %c\n", estado_2);
+    scanf(" %c", &estado_2);
+    
     printf("Código: %s\n", codigo_Carta_2);
+    scanf(" %s", codigo_Carta_2);
+    
     printf("Nome da Cidade: %s\n", nome_Cidade_2);
+    scanf(" %c[^\n]", nome_Cidade_2);
+    
     printf("População: %d Milhões de pessoas\n", populacao_2);
+    scanf (" %d", &populacao_2);
+    
     printf("Área: %.2f Km²\n", area_2);
+    scanf ("%f", &area_2);
+    
     printf("PIB: %.1f Trilhões de Reais\n", pib_2);
+    scanf ("%f", &pib_2);
+    
     printf("Número de Pontos Turísticos: %d Pontos Turísticos\n", pontos_turisticos_2);
+    scanf ("%d", &pontos_turiticos_2);
+    
     printf("Densidade Populacional: %.3f  Habitantes/km²\n", densidade_populacional_2);
+    
+    
     printf("PIB Per Capital: %.3f Reais\n", pib_per_capita_2);
 
 
