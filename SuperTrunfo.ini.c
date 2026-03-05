fada4f4bb9499dcbfd58065d675263d510a9b98f
jczzk <santosjaciel710@icloud.com>
2026-03-05 19:24:48 -0300

    Update SuperTrunfo.ini.c

--- SuperTrunfo.ini.c
+++ SuperTrunfo.ini.c
@@ -3,28 +3,28 @@
 int main() {
 
     // =========================
-    // CARTA 1 - 
+    // CARTA 1 - Rio de Janeiro
     // =========================
 
-    char estado_1 = ;
-    char codigo_Carta_1[22] = ;
-    char nome_Cidade_1[50] = ;
-    int populacao_1 = ;             // Estimativa 2026
-    float area_1 =  ;               // km²
-    float pib_1 = ;                 // Trilhões de reais (Estimativa 2026)
-    int pontos_turisticos_1 = ;
+    char estado_1 = 'A';
+    char codigo_Carta_1[22] = "A01";
+    char nome_Cidade_1[50] = "Rio de Janeiro";
+    int populacao_1 = 11900000;                // Estimativa 2026
+    float area_1 = 1200.25;                    // km²
+    float pib_1 = 1.5;                         // Trilhões de reais (Estimativa 2026)
+    int pontos_turisticos_1 = 45; 
 
     // =========================
-    // CARTA 2 -
+    // CARTA 2 - São Paulo
     // =========================
     
-    char estado_2 = ;
-    char codigo_Carta_2[5] = ;
-    char nome_Cidade_2[50] = ;
-    int populacao_2 = ;              // Estimativa 2026
-    float area_2 = ;                 // km²
-    float pib_2 = ;                // Trilhões de reais (Estimativa 2026)
-    int pontos_turisticos_2 = ;
+    char estado_2 = 'B';
+    char codigo_Carta_2[5] = "B02";
+    char nome_Cidade_2[50] = "São Paulo";
+    int populacao_2 = 11950000;               // Estimativa 2026
+    float area_2 = 1521.11;                   // Km²
+    float pib_2 = 4.3;                        // Trilhões de reais (Estimativa 2026)
+    int pontos_turisticos_2 = 60;
 
 
     // =========================
@@ -37,7 +37,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nome_Cidade_1);
     printf("Populacao: %d Milhões de habitantes\n", populacao_1);
     printf("Area: %.2f Km²\n", area_1);
-    printf("PIB: %.2f Bilhoes de reais\n", pib_1);
+    printf("PIB: R$ %.2f Bilhoes de reais\n", pib_1);
     printf("Numero de Pontos Turisticos: %d\n\n", pontos_turisticos_1);
 
     printf("===== CARTA 2 =====\n");
@@ -46,7 +46,7 @@ int main() {
     printf("Nome da Cidade: %s\n", nome_Cidade_2);
     printf("Populacao: %d Milhões de habitantes\n", populacao_2);
     printf("Area: %.2f Km²\n", area_2);
-    printf("PIB: %.2f Bilhoes de reais\n", pib_2);
+    printf("PIB: R$ %.2f Bilhoes de reais\n", pib_2);
     printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos_2);
 
     return 0;
